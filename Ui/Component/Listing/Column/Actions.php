af6e18c024069865a2879921ba9762abc7351c42
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-11-08 02:40:09 +0100

    Create an admin grid actions column

---
+++ Ui/Component/Listing/Column/Actions.php
@@ -0,0 +1,66 @@
+<?php 
+
+namespace Biglidio\AnsweredQuestions\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class Actions extends Column
+{
+    /** @var UrlInterface */
+    protected $urlBuilder;
+
+    /**
+     * Actions constructor
+     *
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    )
+    {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param array $dataSource
+     *
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource): array
+    {
+        if (!isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
+        foreach($dataSource['data']['items'] as & $item) {
+            if (!isset($item['id'])) {
+                continue;
+            }
+
+            $item[$this->getData('name')] = [
+                'edit' => [
+                    'href' => $this->urlBuilder->getUrl('answeredquestions/faq/edit', [
+                        'id' => $item['id']
+                    ]),
+                    'label' => __('Edit')
+                ],
+            ];
+        }
+
+        return $dataSource;
+    }
+}
